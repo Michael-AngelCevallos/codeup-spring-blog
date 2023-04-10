4906300a5dedb340c7e692185838e9dbc923e548
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-04-10 12:15:02 -0500

    fixed some things with home page and navbar

--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+
+//    this should take you to main page
+
+    @GetMapping("/home")
     public String returnLandingPage() {
         return "index";
     }
